c12dbd276ef80d8548ef86a9b306114828a5c48b
Maravi Jhosep <jhosepe1712@gmail.com>
2026-09-14 18:23:41 -0500

    Implementacion de recursos para empezar el sistema de Punto de Venta

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Punto de Venta Basico</title>
+    <link rel="icon" href="assets/icono.webp">
+</head>
+<body>
+    
+</body>
+</html>
